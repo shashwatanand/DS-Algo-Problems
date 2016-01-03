670c40ef1944810f892f3ee5b69f161d2b5fe85d
Shashwat Anand <shashwat.anand@gmail.com>
2016-01-03 11:55:31 +0530

    Updated Binary Search and First Occurrence and add last occurrence

--- com.shashwat.ArrayProblems/src/com/shashwat/arrayProblems/FirstOccurrence.java
+++ com.shashwat.ArrayProblems/src/com/shashwat/arrayProblems/FirstOccurrence.java
@@ -9,16 +9,20 @@ public class FirstOccurrence {
 	public static void main(String[] args) {
 		int[] array = { 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 4, 5, 5, 6, 7 };
 		int key = 2;
-		System.out.println(key + " is present at " + binarySearch(array, key)
-				+ " index");
+		int keyFoundAt = findFirstOccurrence(array, key);
+		if (keyFoundAt == -1) {
+			System.out.println("Key not found");
+		} else {			
+			System.out.println(key +  " is present at " + keyFoundAt + " index");
+		}
 	}
 
-	private static int binarySearch(int[] array, int key) {
+	private static int findFirstOccurrence(int[] array, int key) {
 		int result = -1;
 		int low = 0;
 		int high = array.length - 1;
-		while (low < high) {
-			int mid = (array[low] + array[high]) / 2;
+		while (low <= high) {
+			int mid = (low + high) / 2;
 			if (key == array[mid]) {
 				result = mid;
 				high = mid - 1;
