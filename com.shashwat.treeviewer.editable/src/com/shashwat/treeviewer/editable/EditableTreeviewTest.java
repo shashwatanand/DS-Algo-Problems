9962a3e751f8ccf654dc0723aec6e4d0fe5b580b
Shashwat Anand <shashwat.anand@gmail.com>
2016-01-16 21:31:31 +0530

    Update

--- com.shashwat.treeviewer.editable/src/com/shashwat/treeviewer/editable/EditableTreeviewTest.java
+++ com.shashwat.treeviewer.editable/src/com/shashwat/treeviewer/editable/EditableTreeviewTest.java
@@ -1,5 +1,23 @@
 package com.shashwat.treeviewer.editable;
 
-public class EditableTreeviewTest {
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
+public class EditableTreeviewTest {
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new FillLayout());
+		new TreeviewerShell(shell);
+		shell.open();
+		
+		while(!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		
+		display.dispose();
+	}
 }
---
+++ com.shashwat.treeviewer.editable/src/com/shashwat/treeviewer/editable/TreeviewerShell.java
@@ -0,0 +1,11 @@
+package com.shashwat.treeviewer.editable;
+
+import org.eclipse.swt.widgets.Shell;
+
+public class TreeviewerShell {
+
+	public TreeviewerShell(Shell shell) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
