f248fae5586849aff7db66581a6199b4d747219d
Shashwat Anand <shashwat.anand@gmail.com>
2016-01-17 11:00:52 +0530

    Modified the parent to ColumnlabelProvider

--- com.shashwat.treeviewer.editable/src/com/shashwat/treeviewer/editable/treeviewSupport/TreeviewerLabelProvider.java
+++ com.shashwat.treeviewer.editable/src/com/shashwat/treeviewer/editable/treeviewSupport/TreeviewerLabelProvider.java
@@ -1,8 +1,8 @@
 package com.shashwat.treeviewer.editable.treeviewSupport;
 
-import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
 
-public class TreeviewerLabelProvider extends LabelProvider {
+public class TreeviewerLabelProvider extends ColumnLabelProvider {
 	@Override
 	public String getText(Object element) {
 		return element.toString();
