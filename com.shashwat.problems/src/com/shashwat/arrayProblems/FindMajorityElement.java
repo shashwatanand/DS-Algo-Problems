83e813e785a1d7182ffe3df5b744fb553779cc08
Shashwat Anand <shashwat.anand@gmail.com>
2016-01-18 20:35:41 +0530

    Update

--- com.shashwat.problems/src/com/shashwat/arrayProblems/FindMajorityElement.java
+++ com.shashwat.problems/src/com/shashwat/arrayProblems/FindMajorityElement.java
@@ -9,6 +9,45 @@ package com.shashwat.arrayProblems;
  */
 public class FindMajorityElement {
 	public static void main(String[] args) {
-		
+		int[] arr = {1, 2, 3, 4 ,4, 4, 4, 4, 5};
+		displayMajorityElement(arr);
+	}
+
+	private static void displayMajorityElement(int[] arr) {
+		int candidate = findCandidate(arr);
+		if (isMajorityElement(candidate, arr)) {
+			System.out.println(candidate);
+		} else {
+			System.out.println("No majority element found");
+		}
+	}
+
+	private static boolean isMajorityElement(int candidate, int[] arr) {
+		int count = 0;
+		for (int index = 0; index < arr.length; index++) {
+			if (arr[index] ==  candidate) {
+				count++;
+			}
+		}
+		if (count > arr.length/2) {
+			return true;
+		}
+		return false;
+	}
+
+	private static int findCandidate(int[] arr) {
+		int cand_index = 0, count = 1;
+		for (int index = 1; index < arr.length; index++) {
+			if (arr[cand_index] == arr[index]) {
+				count++;
+			} else {
+				count--;
+			}
+			if (count == 0) {
+				count = 1;
+				arr[cand_index] = index;
+			}
+		}
+		return arr[cand_index];
 	}
 }
