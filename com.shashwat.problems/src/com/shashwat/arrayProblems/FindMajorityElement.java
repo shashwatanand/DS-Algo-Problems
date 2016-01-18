c51ee00708cccaaa0bbb179aebb88b86d2f88902
Shashwat Anand <shashwat.anand@gmail.com>
2016-01-18 20:25:05 +0530

    Adding New question

---
+++ com.shashwat.problems/src/com/shashwat/arrayProblems/FindMajorityElement.java
@@ -0,0 +1,14 @@
+package com.shashwat.arrayProblems;
+
+/**
+ * Question : A majority element in an array A[] of size n 
+ * is an element that appears more than n/2 times
+ * 
+ * @author Shashwat
+ *
+ */
+public class FindMajorityElement {
+	public static void main(String[] args) {
+		
+	}
+}
