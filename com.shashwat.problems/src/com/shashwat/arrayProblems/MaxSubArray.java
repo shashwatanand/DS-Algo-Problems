ef110b019f94439aae4e40b5627087756b27a854
Shashwat Anand <shashwat.anand@gmail.com>
2016-01-19 19:37:33 +0530

    Update

--- com.shashwat.problems/src/com/shashwat/arrayProblems/MaxSubArray.java
+++ com.shashwat.problems/src/com/shashwat/arrayProblems/MaxSubArray.java
@@ -1,5 +1,26 @@
 package com.shashwat.arrayProblems;
 
+/**
+ * Question :  Find the sum of contiguous subarray within a one-dimensional
+ * array of numbers which has the largest sum. 
+ * 
+ * @author Shashwat
+ *
+ */
 public class MaxSubArray {
+	public static void main(String[] args) {
+		int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+		System.out.println("Maximum contigous sum is " + getMaxSubArraySum(arr));
+	}
 
+	private static int getMaxSubArraySum(int[] arr) {
+		int max = arr[0];
+		int curr_max = arr[0];
+		
+		for (int index = 1; index < arr.length; index++) {
+			curr_max = Math.max(arr[index], curr_max + arr[index]);
+			max = Math.max(max, curr_max);
+		}
+		return max;
+	}
 }
