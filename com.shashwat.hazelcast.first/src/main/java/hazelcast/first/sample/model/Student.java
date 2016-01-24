90f72f41dbb4a49ae24553315573f56e4cc91e24
Shashwat Anand <shashwat.anand@gmail.com>
2016-01-24 21:54:53 +0530

    Adding model class

---
+++ com.shashwat.hazelcast.first/src/main/java/hazelcast/first/sample/model/Student.java
@@ -0,0 +1,48 @@
+package hazelcast.first.sample.model;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+	private String firstName;
+	private String lastName;
+	private String studentId;
+
+	public Student(String firstName, String lastName, String studentId) {
+		super();
+		this.studentId = studentId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @return the studentId
+	 */
+	public String getStudentId() {
+		return studentId;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName
+				+ ", studentId=" + studentId + "]";
+	}
+}
